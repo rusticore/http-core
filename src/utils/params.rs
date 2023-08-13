daa3cea0d40b355ea271e6370f88646ef892f223
Ando <iamando@tuta.io>
2023-08-13 14:57:28 +0300

    feature: check is valid local host params

--- src/utils/params.rs
+++ src/utils/params.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 pub struct Params {}
 
 impl Params {
@@ -6,5 +8,14 @@ impl Params {
     let is_port_empty = port == 0;
 
     is_host_empty && is_port_empty
+  }
+
+  pub fn is_valid_local_host(host: &str) -> bool {
+    let pattern = r"^(localhost|127\.0\.0\.1)$";
+
+    match Regex::new(pattern) {
+      Ok(re) => re.is_match(host),
+      Err(_) => return false,
+    }
   }
 }

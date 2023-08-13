cbdf86df13d6f2baf117a01e758d5ed68fc416e0
Ando <iamando@tuta.io>
2023-08-13 21:26:57 +0300

    feature: re-struct test

--- tests/lib_spec.rs
+++ tests/lib_spec.rs
@@ -1,18 +1,21 @@
-use core::params::Params;
+#[cfg(test)]
+mod params {
+  use core::params::Params;
 
-#[test]
-fn is_empty() {
-  let host = "127.0.0.1";
-  let port = 4000;
-  let result = Params::is_empty(host, port);
+  #[test]
+  fn is_empty() {
+    let host = "127.0.0.1";
+    let port = 4000;
+    let result = Params::is_empty(host, port);
 
-  assert_eq!(result, false);
-}
+    assert_eq!(result, false);
+  }
 
-#[test]
-fn is_valid_local_host() {
-  let host = "127.0.0.1";
-  let result = Params::is_valid_local_host(host);
+  #[test]
+  fn is_valid_local_host() {
+    let host = "127.0.0.1";
+    let result = Params::is_valid_local_host(host);
 
-  assert_eq!(result, true);
+    assert_eq!(result, true);
+  }
 }

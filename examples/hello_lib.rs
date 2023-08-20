27032697bc21bdcf8b8ae2b743acb9a92d0710ff
Ando <iamando@tuta.io>
2023-08-20 18:02:12 +0300

    feature: implement router handling and improve thread on listen

--- examples/hello_lib.rs
+++ examples/hello_lib.rs
@@ -1,9 +1,46 @@
 use core::server::Server;
 
+const HELLO_WORLD_HTML: &str = r#"
+  <!DOCTYPE html>
+  <html>
+  <head>
+      <title>Hello, World!</title>
+  </head>
+  <body>
+      <h1>Hello, World!</h1>
+  </body>
+  </html>
+"#;
+
+const ABOUT_WORLD_HTML: &str = r#"
+  <!DOCTYPE html>
+  <html>
+  <head>
+      <title>About, World!</title>
+  </head>
+  <body>
+      <h1>About, World!</h1>
+  </body>
+  </html>
+"#;
+
 fn main() {
   let host = "127.0.0.1";
   let port = 4000;
-  let server = Server::new();
+  let mut server = Server::new();
+
+  fn hello_world_handler(_: &[u8]) {
+    println!("Handling Hello, World! request");
+    println!("{}", HELLO_WORLD_HTML);
+  }
+
+  fn about_world_handler(_: &[u8]) {
+    println!("Handling About, World! request");
+    println!("{}", ABOUT_WORLD_HTML);
+  }
+
+  server.routes("/", hello_world_handler);
+  server.routes("/about", about_world_handler);
 
   server.listen(host, port);
 }

753723b4e9da30284fdb0e2e0aa377c9d83a3583
Ando <iamando@tuta.io>
2024-02-09 21:28:52 +0300

    feat: refact codebase and use match on tcp listener accept in server lib

--- src/handler.rs
+++ src/handler.rs
@@ -14,16 +14,13 @@ impl Handler {
   pub fn handle_connection(mut tcp_stream: TcpStream, router: &Box<dyn Fn(&[u8]) + Send + Sync>) {
     loop {
       let mut buffer = [0; 1028];
-
       match tcp_stream.read(&mut buffer) {
         Ok(stream_count) => {
           if stream_count == 0 {
             break;
           }
-
           let request = &buffer[0..stream_count];
           router(request);
-
           println!("{}", Handler::get_request_path(request));
         }
         Err(e) => panic!("{}", e),
@@ -35,7 +32,6 @@ impl Handler {
     if let Err(e) = ctrlc::set_handler(move || {
       exit.store(true, Ordering::SeqCst);
       println!("Ctrl + C pressed. Exiting gracefully...");
-
       process::exit(0);
     }) {
       panic!("{}", e);
@@ -45,7 +41,6 @@ impl Handler {
   pub fn get_request_path(request: &[u8]) -> String {
     let request_str = String::from_utf8_lossy(request);
     let request_lines: Vec<&str> = request_str.lines().collect();
-
     let request_path = request_lines
       .first()
       .unwrap_or(&"")
--- src/params.rs
+++ src/params.rs
@@ -4,7 +4,6 @@ use regex::Regex;
 pub fn is_empty(host: &str, port: u32) -> bool {
   let is_host_empty = host.is_empty();
   let is_port_empty = port == 0;
-
   match (is_host_empty, is_port_empty) {
     (true, true) => true,
     _ => false,
@@ -13,7 +12,6 @@ pub fn is_empty(host: &str, port: u32) -> bool {
 
 pub fn is_valid_local_host(host: &str) -> bool {
   let pattern = constants::HOST_REGEX;
-
   match Regex::new(pattern) {
     Ok(re) => re.is_match(host),
     Err(_) => false,
@@ -22,7 +20,6 @@ pub fn is_valid_local_host(host: &str) -> bool {
 
 pub fn is_valid_port(port: u32) -> bool {
   let pattern = constants::PORT_REGEX;
-
   match Regex::new(pattern) {
     Ok(re) => re.is_match(&port.to_string()),
     Err(_) => false,
